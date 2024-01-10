4c7717b204334c42380757ea7f714b76a1aecfb5
Michael Inma <112844696+Divorso@users.noreply.github.com>
2024-01-10 22:28:39 +0700

    ändrat från " till '

--- src/timeCount.ts
+++ src/timeCount.ts
@@ -1,4 +1,4 @@
-import { answeredQuestionsCount } from "./answerCurrentQuestion";
+import { answeredQuestionsCount } from './answerCurrentQuestion';
 
 const timeCount: HTMLDivElement | null = document.querySelector('#timeCount');
 const msg: HTMLDivElement | null = document.querySelector('#msg');
@@ -6,36 +6,35 @@ const quizBox: HTMLDivElement | null = document.querySelector('#quizbox');
 const quizStartCon: HTMLDivElement | null = document.querySelector('#quiz-start');
 
 function startQuiz() {
-    if (quizBox && quizStartCon && msg) {
-        quizStartCon.classList.add('hidden');
+  if (quizBox && quizStartCon && msg) {
+    quizStartCon.classList.add('hidden');
 
-        msg.innerHTML = `<div class="msg">
+    msg.innerHTML = `<div class="msg">
         <p>Spelet börjar nu!</p>
         </div>
         `;
 
-        setTimeout(() => {
-            if (msg) {
-                msg.classList.add('hidden'); 
-            }
-            quizBox.classList.remove('hidden');
-            
-        }, 1500);
+    setTimeout(() => {
+      if (msg) {
+        msg.classList.add('hidden');
+      }
+      quizBox.classList.remove('hidden');
+    }, 1500);
 
-        let intervalCount = 0;
+    let intervalCount = 0;
 
-       const countInterval = setInterval(timeQuiz,1000);
+    const countInterval = setInterval(timeQuiz, 1000);
 
-        function timeQuiz() {
-            intervalCount += 0.1;
-            if (timeCount) {
-                timeCount.innerHTML = `<div> Tid: ${intervalCount.toFixed(1)}</div>`;
-            } 
-            if(answeredQuestionsCount >= 10){
-                clearInterval(countInterval);
-            }
-        }
+    function timeQuiz() {
+      intervalCount += 0.1;
+      if (timeCount) {
+        timeCount.innerHTML = `<div> Tid: ${intervalCount.toFixed(1)}</div>`;
+      }
+      if (answeredQuestionsCount >= 10) {
+        clearInterval(countInterval);
+      }
     }
+  }
 }
 
 export default startQuiz;
