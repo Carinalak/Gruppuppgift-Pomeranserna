7605b892f89baf6e39ab2a81903e5e52fe8e098e
Michael Inma <112844696+Divorso@users.noreply.github.com>
2024-01-12 20:00:00 +0700

    Update endMessage.ts
    Rättade till stavfel

--- src/helpers/endMessage.ts
+++ src/helpers/endMessage.ts
@@ -2,11 +2,11 @@ import { playerArray } from '../data/variable';
 
 export function endMessage(value: number): string {
   if (value >= 0 && value <= 3) {
-    return `Ajdå ${playerArray.playerName}, Bättre kan du! Testa igen!`;
+    return `Ajdå ${playerArray.playerName}, bättre kan du! Testa igen!`;
   } else if (value >= 4 && value <= 6) {
-    return `Grymt jobbat ${playerArray.playerName}! Men de känns som att du kan bättre.`;
+    return `Grymt jobbat ${playerArray.playerName}!, men de känns som att du kan bättre.`;
   } else if (value >= 7 && value <= 9) {
-    return `Bra jobbat ${playerArray.playerName}, Vad grym du är!!`;
+    return `Bra jobbat ${playerArray.playerName}, vad grym du är!!`;
   } else if (value === 10) {
     return `${playerArray.playerName} är bäst! alla rätt! whoop whoop!`;
   } else {
