ff7179a4c4130b4e26a7d155dc5bde606bdf1cc2
Michael Inma <112844696+Divorso@users.noreply.github.com>
2024-01-12 20:24:15 +0700

    rättade till stavfel
    rättade till stavfel

--- src/helpers/endMessage.ts
+++ src/helpers/endMessage.ts
@@ -4,7 +4,7 @@ export function endMessage(value: number): string {
   if (value >= 0 && value <= 3) {
     return `Ajdå ${playerArray.playerName}, bättre kan du! Testa igen!`;
   } else if (value >= 4 && value <= 6) {
-    return `Grymt jobbat ${playerArray.playerName}!, men de känns som att du kan bättre.`;
+    return `Grymt jobbat ${playerArray.playerName}! Men de känns som att du kan bättre.`;
   } else if (value >= 7 && value <= 9) {
     return `Bra jobbat ${playerArray.playerName}, vad grym du är!!`;
   } else if (value === 10) {
